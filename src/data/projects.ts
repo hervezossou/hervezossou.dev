4d875be3d875879fb30f5f5c8b1bca2c0df58a6c
Hervé Zossou <hervezossou8@gmail.com>
2025-05-13 10:43:00 +0100

    update projects images path

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,22 +1,26 @@
+const kemacoCover = "/kemaco-cover.png";
+const ballamasCover = "/ballamas-cover.png";
+const tokenaCover = "/tokena-cover.png";
+
 export const projects = [
     {
       title: 'Kemaco Sarl Website',
       description: 'A website for a company that specializes in building modern construction using shipping container. It showcases their services with a modern design, easy navigation, and a responsive layout.',
-      image: "/kemaco-cover.png",
+      image: kemacoCover,
       tags: ['NextJs', 'TypeScript', 'TailwindCSS', 'Node.js', 'PostgreSQL'],
       url: 'https://kemaco.pro/'
     },
     {
       title: 'Ballamas - E-commerce',
       description: 'A landing page for an e-commerce website that sells sports equipment and apparel. The page features a clean and modern design for a seamless shopping experience.',
-      image: "/ballamas-cover.png",
+      image: ballamasCover,
       tags: ['HTML', 'CSS', 'JavaScript', 'Mock.shop API'],
       url: 'https://figma-to-code-ed2-week2-gamma.vercel.app/'
     },
     {
       title: 'Tokena - Crypto Tracker',
       description: 'Tokena is a cryptocurrency tracker that provides real-time data on various cryptocurrencies, including their prices, market capitalization, and trading volume with advanced features.',
-      image: "/tokena-cover.png",
+      image: tokenaCover,
       tags: ['HTML', 'CSS', 'JavaScript', 'Coingecko API'],
       url: 'https://figma-to-code-ed2-week3-topaz.vercel.app/'
     }
